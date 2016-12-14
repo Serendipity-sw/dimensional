8440d7c17a0fa9691f385b139ff7dbb51c82555d
gloomysw <gloomysw>
2016-12-14 11:53:31 +0800

    自动化下载程序初步完工

--- serverRun.go
+++ serverRun.go
@@ -5,7 +5,9 @@ import (
 	"github.com/ddliu/go-httpclient"
 	"github.com/smtc/glog"
 	"goquery"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -154,8 +156,99 @@ func analysisFollowUserCOSEveryPage(followUserCOSPageUrl string, pageNumber int)
 			glog.Error("analysisFollowUserCOSEveryPage analysis documenterr! sendHttp: %s  err: %s \n", httpUrl, err.Error())
 			return
 		}
-		doc.Find(".l-grid__inner li").Each(func() {
-
+		doc.Find(".l-grid__inner li").Each(func(indexNumber int, nodeObj *goquery.Selection) {
+			hrefUrlPath, bo := nodeObj.Find("a").First().Attr("href")
+			if bo {
+				userSendPostsProcess(fmt.Sprintf("http://bcy.net%s", hrefUrlPath))
+			}
 		})
 	}
+}
+
+/**
+用户发的每一页cos页面图片下载
+创建人:邵炜
+创建时间:2016年12月14日11:18:10
+输入参数:用户COS帖子页面地址
+*/
+func userSendPostsProcess(userCOSPostsUrlPath string) {
+	attentionPage, err := httpclient.WithCookie(cookies...).Get(userCOSPostsUrlPath, nil)
+	if err != nil {
+		glog.Error("userSendPostsProcess send http err! httpUrl: %s err: %s \n", userCOSPostsUrlPath, err.Error())
+		return
+	}
+	doc, err := goquery.NewDocumentFromReader(attentionPage.Body)
+	attentionPage.Body.Close()
+	if err != nil {
+		glog.Error("userSendPostsProcess read body err! userCOSPostsUrlPath: %s err: %s \n", userCOSPostsUrlPath, err.Error())
+		return
+	}
+	userName := doc.Find(".js-userTpl").Find(".fz14.blue1").First().Text()
+	mkdirPath := fmt.Sprintf("./COS/%s", userName)
+	err = os.MkdirAll(mkdirPath, 0777)
+	if err != nil {
+		glog.Error("userSendPostsProcess create file err! mkdirPath: %s err: %s \n", mkdirPath, err.Error())
+		return
+	}
+	mkdirPathFileName := doc.Find(".js-post-title").First().Text()
+	mkdirPathFileNamePath := fmt.Sprintf("%s/%s", mkdirPath, mkdirPathFileName)
+	bo, _ := pathExists(mkdirPathFileNamePath)
+	if !bo {
+		return
+	}
+	err = os.MkdirAll(mkdirPathFileNamePath, 0777)
+	if err != nil {
+		glog.Error("userSendPostsProcess userCOSPosts exsis! mkdirPathFileNamePath: %s err: %s \n", mkdirPathFileNamePath, err.Error())
+		return
+	}
+	doc.Find(".content-img-wrap-inner").Each(func(indexNumber int, nodeObj *goquery.Selection) {
+		COSPictureUrlStr, bo := nodeObj.Find("a").First().Attr("href")
+		if bo {
+			COSPictureUrlArray := strings.Split(COSPictureUrlStr, "&url=")
+			if len(COSPictureUrlArray) != 2 {
+				glog.Error("userSendPostsProcess picuteUrl err! COSPictureUrlStr: %s \n", COSPictureUrlStr)
+			} else {
+				pictureDown(COSPictureUrlArray[1], mkdirPathFileNamePath)
+			}
+		}
+	})
+}
+
+/**
+图片下载
+创建人:邵炜
+创建时间:2016年12月14日11:46:36
+输入参数:图片地址 初始目录地址
+*/
+func pictureDown(urlPathStr, mkdirPath string) {
+	res, err := http.Get(urlPathStr)
+	if err != nil {
+		glog.Error("pictureDown send http err! urlPathStr: %s err: %s \n", urlPathStr, err.Error())
+		return
+	}
+	urlPathArray := strings.Split(urlPathStr, "/")
+	pictureFileName := urlPathArray[len(urlPathArray)-1]
+	picuteCreatePathStr := fmt.Sprintf("%s/%s", mkdirPath, pictureFileName)
+	file, err := os.Create(picuteCreatePathStr)
+	if err != nil {
+		glog.Error("pictureDown picute create err! filePath: %s err: %s \n", picuteCreatePathStr, err.Error())
+		return
+	}
+	io.Copy(file, res.Body)
+}
+
+/**
+判断当前文件夹是否存在
+创建人:邵炜
+创建时间:2016年12月14日11:23:01
+*/
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
